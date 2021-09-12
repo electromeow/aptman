6b6fad12790425d50227c97e9be53a24a4a2eb92
electromeow <gokgoz.berat123@gmail.com>
2021-09-12 15:10:01 +0300

    Solve TypeError

--- install.py
+++ install.py
@@ -3,6 +3,6 @@ import os
 
 os.system("touch /usr/local/bin/aptman")
 f = open("/usr/local/bin/aptman", 'w')
-f.write(url.urlopen(url.Request("https://raw.githubusercontent.com/electromeow/aptman/master/main.py",headers={"User-Agent": "Mozilla/5.0"})).read())
+f.write(url.urlopen(url.Request("https://raw.githubusercontent.com/electromeow/aptman/master/main.py",headers={"User-Agent": "Mozilla/5.0"})).read().decode("utf-8"))
 f.close()
 os.system("chmod +x /usr/local/bin/aptman")
